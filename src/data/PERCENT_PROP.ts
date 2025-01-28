b290dea94ab9a8717fd84972efbeb10029072f16
LabeebHameed <vhlabeeb@gmail.com>
2025-01-28 19:26:16 +0530

    Delete src directory

--- src/data/PERCENT_PROP.ts
+++
@@ -1,5 +0,0 @@
-export const Dash_stats = [
-  { label: "Temperature", value: 52, color: "bg-orange-500" },
-  { label: "Humidity", value: 21, color: "bg-green-500" },
-  { label: "Water Level", value: 74, color: "bg-blue-500" },
-];
