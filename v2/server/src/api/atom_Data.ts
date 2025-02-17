b41c4e854b655ddb401c07b1eb70fbd42c0ddb0a
LabeebHameed <vhlabeeb@gmail.com>
2025-02-17 11:30:52 +0530

    Delete v2 directory

--- v2/server/src/api/atom_Data.ts
+++
@@ -1,7 +0,0 @@
-// server/src/data/atom_Data.ts
-export const atom_Data = [
-    { label: "Temperature", value: 52, color: "bg-blue-500" },
-    { label: "Humidity", value: 10, color: "bg-blue-500" },
-    { label: "Water Level", value: 17, color: "bg-blue-500" },
-  ];
-  
--- v2/server/src/firebaseAdmin.ts
+++
@@ -1,9 +0,0 @@
-// firebaseAdmin.js
-import admin from 'firebase-admin';
-import serviceAccount from './caria-9c751-firebase-adminsdk-fbsvc-c38b833792.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
-
-module.exports = admin;
